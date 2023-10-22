b1c7e85cc8059efd997c6ec7da9bc91417148c18
Salah-ZEddine <146392495+Salah-ZEddine@users.noreply.github.com>
2023-10-22 19:14:29 +0100

    cmp linguistiques

--- cv-react/src/App.tsx
+++ cv-react/src/App.tsx
@@ -1,5 +1,6 @@
 import InfoPers from "./components/Infopers"
 import Footer from "./components/Footer"
+import Cmlg from "./components/Cmlg"
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <InfoPers name="Salah Eddine" bdate="01/01/2003" bloca="Casablanca" sf="Célibataire"/>
+      <Cmlg/>
       <Footer/>
     </>
   )
