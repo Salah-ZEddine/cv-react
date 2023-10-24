4c139a946938f01978c27a48175882b191127622
Salah-ZEddine <146392495+Salah-ZEddine@users.noreply.github.com>
2023-10-24 19:53:29 +0100

    ajout de ojectif personnel

--- cv-react/src/App.tsx
+++ cv-react/src/App.tsx
@@ -2,6 +2,7 @@ import InfoPers from "./components/Infopers"
 import Footer from "./components/Footer"
 import Cmlg from "./components/Cmlg"
 import Cmtc from "./components/Cmtc"
+import ObjProf from "./components/ObjProf"
 import './App.css'
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <InfoPers name="Salah Eddine" bdate="01/01/2003" bloca="Casablanca" sf="Célibataire"/>
       <Cmlg/>
       <Cmtc/>
+      <ObjProf/>
       <Footer/>
     </>
   )
